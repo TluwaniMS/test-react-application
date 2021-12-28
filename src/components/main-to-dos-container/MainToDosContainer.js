3acca54ba73d7607743b13bc6100f922823ebb02
tluwaniMS <tluwani2020@gmail.com>
2021-12-28 10:05:44 +0200

    adding-sample-to-dos

--- src/components/main-to-dos-container/MainToDosContainer.js
+++ src/components/main-to-dos-container/MainToDosContainer.js
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 const MainToDosContainer = (props) => {
+  const [todos, modifyToDos] = useState([
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" },
+    { task: "", dateToBeCompleted: "", id: "", dateCompleted: "", status: "" }
+  ]);
+  
   return <div></div>;
 };
 
