93888003757947a612035584847150d7def75521
tluwaniMS <tluwani2020@gmail.com>
2021-12-30 09:37:04 +0200

    adding-municipality-query-logic

--- src/queries/municipalities.query.js
+++ src/queries/municipalities.query.js
@@ -1,5 +1,17 @@
 import { gql } from "@apollo/client";
 
-export const GET_ALL_MUNICIPALITIES = gql``;
+export const GET_ALL_MUNICIPALITIES = gql`
+  query getAllMunicipalities {
+    municipalityName
+    municipalityKey
+  }
+`;
 
-export const GET_MUNICIPALITY_BY_KEY = gql``;
+export const GET_MUNICIPALITY_BY_KEY = gql`
+  query getMunicipalityByKey($municipalityKey: String!) {
+    getMunicipalityByMunicipalityKey(municipalityKey: $municipalityKey) {
+      municipalityName
+      municipalityKey
+    }
+  }
+`;
