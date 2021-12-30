57901464d3c7b07e1940493b36dadde40ec78674
tluwaniMS <tluwani2020@gmail.com>
2021-12-30 08:56:12 +0200

    adding-doctors-query-logic

--- src/queries/doctors.query.js
+++ src/queries/doctors.query.js
@@ -1,5 +1,22 @@
 import { gql } from "@apollo/client";
 
-export const GET_ALL_DOCTORS = gql``;
+export const GET_ALL_DOCTORS = gql`
+  query getAllDoctors {
+    id
+    firstName
+    lastName
+  }
+`;
 
-export const GET_DOCTOR_BY_ID = gql``;
+export const GET_DOCTOR_BY_ID = gql`
+  query getDoctorById($doctorsId: Int!) {
+    getDoctorById(doctorsId: $doctorsId) {
+      id
+      firstName
+      lastName
+      gender
+      email
+      hospital
+    }
+  }
+`;
