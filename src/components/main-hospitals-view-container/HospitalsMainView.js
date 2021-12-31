b1aeb08c3c006c1530aa74650ed548ab98aeae2d
tluwaniMS <tluwani2020@gmail.com>
2021-12-31 08:00:40 +0200

    adding-queries-to-main-views

--- src/components/main-hospitals-view-container/HospitalsMainView.js
+++ src/components/main-hospitals-view-container/HospitalsMainView.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_HOSPITALS } from "../../queries/hospitals.query";
 
 const HospitalsMainView = () => {
+  const { loading, error, data } = useQuery(GET_ALL_HOSPITALS);
+
   return (
     <div>
       <h1>Hospitals view works!!</h1>
--- src/components/main-municipalities-view-container/MunicipalitiesMainView.js
+++ src/components/main-municipalities-view-container/MunicipalitiesMainView.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_MUNICIPALITIES } from "../../queries/municipalities.query";
 
 const MunicipalitiesMainView = () => {
+  const { loading, error, data } = useQuery(GET_ALL_MUNICIPALITIES);
+
   return (
     <div>
       <h1>Municipalities view works!!</h1>
