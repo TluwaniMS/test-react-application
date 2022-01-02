a4ff38f26341cacec2a4efffaf31f16ada29eb39
tluwaniMS <tluwani2020@gmail.com>
2022-01-02 15:46:08 +0200

    adding-detailed-view-routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./components/nav-bar/Navbar";
 import DoctorsMainView from "./components/main-doctors-view-container/DoctorsMainView";
 import HospitalsMainView from "./components/main-hospitals-view-container/HospitalsMainView";
 import MunicipalitiesMainView from "./components/main-municipalities-view-container/MunicipalitiesMainView";
+import DetailedDoctorsView from "./views/detailed-doctors-view/DetailedDoctorsView";
+import DetailedHospitalsView from "./views/detailed-hospitals-view/DetailedHospitalsView";
+import DetailedMunicipalitiesView from "./views/detailed-municipalities-view/DetailedMunicipalitiesView";
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
         <Route path="/" element={<MunicipalitiesMainView />} />
         <Route path="/municipalities" element={<MunicipalitiesMainView />} />
         <Route path="/doctors" element={<DoctorsMainView />} />
-        <Route path="/hospitals" element={<HospitalsMainView />} />=
+        <Route path="/hospitals" element={<HospitalsMainView />} />
+        <Route path="/detailed-municipality-view/:municipalityKey" element={<DetailedMunicipalitiesView />} />
+        <Route path="/detailed-hospital-view/:hospitalKey" element={<DetailedHospitalsView />} />
+        <Route path="/detailed-doctors-view/:doctorId" element={<DetailedDoctorsView />} />
       </Routes>
     </div>
   );
