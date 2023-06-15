8270eea2fb5e30a887a3fea86461ea7072216879
tluwaniMS <tluwani2020@gmail.com>
2023-06-15 06:04:24 +0200

    updating-server-config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+const host = process.env.REACT_APP_SERVER || `localhost`;
 
 const client = new ApolloClient({
-  uri: "http://localhost:5002/graphql",
-  cache: new InMemoryCache()
+  uri: `http://${host}:5002/graphql`,
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
@@ -19,7 +21,7 @@ ReactDOM.render(
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 reportWebVitals();
