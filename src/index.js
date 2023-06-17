d815c61f740db952fa2248f64671172549c6c1f9
tluwaniMS <tluwani2020@gmail.com>
2023-06-17 16:20:25 +0200

    updating-host-reader

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 
+const host = process.env.REACT_APP_SERVER_HOST || 'localhost';
+
 const client = new ApolloClient({
-  uri: `http://localhost:5002/graphql`,
+  uri: `http://${host}:5002/graphql`,
   cache: new InMemoryCache(),
 });
 
